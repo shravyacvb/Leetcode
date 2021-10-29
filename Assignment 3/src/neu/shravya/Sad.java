2ad7658301c4e319108575dbd13e1f135d5083e3
shravyacvb <cvbshravya@gmail.com>
2021-10-29 01:08:03 -0700

    Assignment 3

---
+++ Assignment 3/src/neu/shravya/Sad.java
@@ -0,0 +1,19 @@
+package neu.shravya;
+
+public class Sad extends Moody{
+
+    @Override
+    protected String getMood() {
+        return "I feel sad today";
+    }
+
+    @Override
+    public void expressFeelings() {
+        System.out.println("""
+                ‘waah’ ‘boo hoo’ ‘weep’ ‘sob’""");
+    }
+
+    public String toString() {
+        return "Subject cries a lot";
+    }
+}
